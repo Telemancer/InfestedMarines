5d6c600ab7ce0ed61945ad1cf8a1b1330ded2f1e
David Drane <telemancer0@gmail.com>
2020-10-23 02:31:28 -0400

    Update IMCyst_Server.lua

--- InfestedMarines/source/lua/IMCyst_Server.lua
+++ InfestedMarines/source/lua/IMCyst_Server.lua
@@ -13,16 +13,24 @@ end
 function Cyst:OnEntityChange(entityId, newEntityId)
 end
 
-local old_Cyst_OnKill = Cyst.OnKill
 function Cyst:OnKill(attacker, doer, point, direction)
     
-    if doer and (doer:isa("Flamethrower") or doer:isa("Welder")) then
+    -- Handle OnKill for this cyst
+	self:TriggerEffects("death")
+	self.connected = false
+	self:SetModel(nil)
+	self:Kill()
+	
+    if doer and doer:isa("Flamethrower") then
         -- kill all cysts within IMCystManager.kCystConnectionRadius, to make clearing easier
         -- and also as a workaround for the unreachable cyst issue.
         local cysts = GetEntitiesWithinRange("Cyst", self:GetOrigin(), IMCystManager.kCystConnectionRadius)
         for i=1, #cysts do
-            if cysts[i] and cysts[i] ~= self and cysts[i].GetIsAlive and cysts[i]:GetIsAlive() then
-                cysts[i]:Kill()
+            if cysts[i] and cysts[i] ~= self and cysts[i].GetIsAlive then
+				cysts[i]:TriggerEffects("death")
+				cysts[i].connected = false
+				cysts[i]:SetModel(nil)
+				cysts[i]:Kill()
             end
         end
     end
@@ -30,8 +38,6 @@ function Cyst:OnKill(attacker, doer, point, direction)
     GetCystManager():CreateAreaOfDenial(self:GetOrigin())
     
     TipHandler_ReportCystKilled(attacker)
-    
-    old_Cyst_OnKill(self, attacker, doer, point, direction)
     
 end
 
